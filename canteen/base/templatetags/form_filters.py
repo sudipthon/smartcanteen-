58603213791133a8807c8393b575e77b955cb9a5
shushmaaaa <shushmakhattri382@gmail.com>
2024-06-19 11:48:35 +0545

    update

--- canteen/base/templatetags/form_filters.py
+++ canteen/base/templatetags/form_filters.py
@@ -6,3 +6,10 @@ register = template.Library()
 @register.filter
 def get_form(forms, day):
     return forms.get(day)
+
+@register.filter
+def multiply(value, arg):
+    try:
+        return value * arg
+    except (ValueError, TypeError):
+        return ''

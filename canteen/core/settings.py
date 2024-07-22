4dd8fe994a6269a3934ed946b78c05b1182cacc9
sudip <sudip00kc@gmail.com>
2024-07-22 07:11:10 +0545

    dock configured

--- canteen/core/settings.py
+++ canteen/core/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "unfold",
+    # "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

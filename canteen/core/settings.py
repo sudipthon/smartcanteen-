b131e9acd6dfbaa0f6735a2e4cdb6ff4fa091316
sudipthon <sudip00kc@gmail.com>
2024-08-05 12:13:41 +0545

    had to change the trusted origins

--- canteen/core/settings.py
+++ canteen/core/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 AUTH_USER_MODEL = "base.CustomUser"
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://smartcanteen-cyij.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://smartcanteen-ni2a.onrender.com"]
 
 
 # Application definition
